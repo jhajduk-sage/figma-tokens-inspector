76b6cb1435bd0292ec95d07aee66b6e58985c349
Jakub Hajduk <ext-jakub.hajduk@sage.com>
2021-10-12 12:58:05 +0200

    Added translating properties to css properties

--- src/code.tsx
+++ src/code.tsx
@@ -1,45 +1,63 @@
-figma.showUI(__html__, {width: 350, height: 150});
+figma.showUI(__html__, {width: 500, height: 300});
 
-const properties = {
-  values: {},
-  sizing: {},
-  height: {},
-  width: {},
-  spacing: {},
-  verticalPadding: {},
-  horizontalPadding: {},
-  itemSpacing: {},
-  fill: {},
-  border: {},
-  borderRadius: {},
-  borderRadiusTopLeft: {},
-  borderRadiusTopRight: {},
-  borderRadiusBottomRight: {},
-  borderRadiusBottomLeft: {},
-  borderWidth: {},
-  boxShadow: {},
-  opacity: {},
-  fontFamilies: {},
-  fontWeights: {},
-  fontSizes: {},
-  lineHeights: {},
-  typography: {},
-  letterSpacing: {},
-  paragraphSpacing: {},
-  tokenValue: {},
-  tokenName: {},
-  description: {},
+const propertiesMapping = {
+  sizing: ['width and height'],
+  height: 'height',
+  width: 'width',
+  spacing: 'padding',
+  verticalPadding: 'padding left and right',
+  horizontalPadding: 'padding top and bottom',
+  itemSpacing: 'padding',
+  fill: ['background-color', 'color'],
+  border: 'border-color',
+  borderRadius: 'border-radius',
+  borderRadiusTopLeft: 'border-top-left-radius',
+  borderRadiusTopRight: 'border-top-right-radius',
+  borderRadiusBottomRight: 'border-bottom-right-radius',
+  borderRadiusBottomLeft: 'border-bottom-left-radius',
+  borderWidth: 'border-width',
+  boxShadow: 'box-shadow',
+  opacity: 'opacity',
+  fontFamilies: 'font-family',
+  fontWeights: 'font-weight',
+  fontSizes: 'font-size',
+  lineHeights: 'line-height',
+  typography: 'font',
+  letterSpacing: 'letter-spacing',
+  paragraphSpacing: 'text-indent',
+  paddingTop: 'padding-top',
+  paddingBottom: 'padding-bottom',
+  paddingLeft: 'padding-left',
+  paddingRight: 'padding-right'
 };
+
 
 export function getTokenName(node: BaseNode, property: string) {
   if (!node) return null;
-  const output = node.getSharedPluginData('tokens', property) || node.getPluginData(property);
+  const output = node.getSharedPluginData('tokens', property);
   return output ? JSON.parse(output) : null;
 }
 
+export function mapToCssProperty(node: BaseNode, property: string) {
+  if (!node) return null;
+
+  if (node.type === 'TEXT' && property === 'fill') {
+    return propertiesMapping[property][1];
+  }
+
+  if (property === 'fill') {
+    return propertiesMapping[property][0];
+  }
+
+  return propertiesMapping[property];
+}
+
 export function getAssignedTokens(node: BaseNode) {
-  const pluginData = Object.keys(properties)
-      .map((prop) => [prop, getTokenName(node, prop)])
+  const pluginData = Object.keys(propertiesMapping)
+      .map((prop) => [
+        mapToCssProperty(node, prop),
+        getTokenName(node, prop)
+      ])
       .filter(([, value]) => !!value);
   return pluginData.length > 0 ? Object.fromEntries(pluginData) : null;
 }
